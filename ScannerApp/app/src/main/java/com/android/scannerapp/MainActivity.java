ada774409a99593b53500cf0fcf7b8cdc35c4729
trampham <pham1942000@gmail.com>
2020-11-09 22:11:24 +0700

    Camera button

--- ScannerApp/app/src/main/java/com/android/scannerapp/MainActivity.java
+++ ScannerApp/app/src/main/java/com/android/scannerapp/MainActivity.java
@@ -2,13 +2,32 @@ package com.android.scannerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton btnCamera;
+    int REQUEST_CODE=123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Màn hình chụp ảnh
+        btnCamera=(ImageButton)findViewById(R.id.btn_Camera);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,REQUEST_CODE);
+            }
+        });
     }
+
+
 }
