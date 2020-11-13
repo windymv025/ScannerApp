a61cb4d8ffd7a3a3f33f2f8362ed9996644fc971
minhtri2404 <hokhacminhtri@gmail.com>
2020-11-13 19:12:55 +0700

    load image from gallery

---
+++ ScannerApp/app/src/main/java/com/android/scannerapp/LoadImage.java
@@ -0,0 +1,14 @@
+package com.android.scannerapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import static android.app.Activity.RESULT_OK;
+import static androidx.core.app.ActivityCompat.startActivityForResult;
+
+public class LoadImage {
+
+}
--- ScannerApp/app/src/main/java/com/android/scannerapp/MainActivity.java
+++ ScannerApp/app/src/main/java/com/android/scannerapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.android.scannerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -14,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
 
     ImageButton btnCamera;
     int REQUEST_CODE=123;
+
+    ImageView imageView;
+    Button button;
+    private static final int PICK_IMAGE = 1;
+    Uri imageUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +34,31 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent,REQUEST_CODE);
             }
         });
+
+        // load image from gallery
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.buttonLoadPicture);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
     }
 
+    // open gallery
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
+    }
 
 }
