f6b7f15fc89b7b378d3b117ccbdb3be0da393822
minhtri2404 <hokhacminhtri@gmail.com>
2021-01-10 03:00:52 +0700

    update read-show-pdf-file code

--- ScannerApp/app/src/main/java/com/android/scannerapp/PdfActivity.java
+++ ScannerApp/app/src/main/java/com/android/scannerapp/PdfActivity.java
@@ -13,7 +13,8 @@ import java.io.File;
 public class PdfActivity extends AppCompatActivity {
     PDFView pdfView;
     File file;
-    private final int CHOOSE_PDF_FROM_FILE = 100;
+    Intent intent;
+    private final int CHOOSE_PDF_FROM_FILE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,24 +22,45 @@ public class PdfActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_pdf);
 
         pdfView = (PDFView) findViewById(R.id.pdf_view);
-        callChooseFileFromDevice();
+        callChoosePdfFile();
     }
 
-    private void callChooseFileFromDevice() {
-        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
+    public void callChoosePdfFile() {
+//        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+//        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent = new Intent (Intent.ACTION_GET_CONTENT);
         intent.setType("application/pdf");
         startActivityForResult(intent, CHOOSE_PDF_FROM_FILE);
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
-        super.onActivityResult(requestCode, resultCode, resultData);
-        if(requestCode == CHOOSE_PDF_FROM_FILE && resultCode == RESULT_OK) {
-            if(resultData != null) {
-                file = new File(String.valueOf(resultData.getData()));
-                pdfView.fromFile(file).load();
-            }
+    protected void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+//        super.onActivityResult(requestCode, resultCode, resultData);
+//        if(requestCode == CHOOSE_PDF_FROM_FILE && resultCode == RESULT_OK) {
+//            if(resultData != null) {
+//                String path = resultData.getData().getPath();
+//                Log.i("TEST", path);
+//                file = new File(path);
+//
+////                pdfView.fromFile(file).load();
+//                pdfView.fromAsset("pdf_book.pdf").load();
+//
+//            }
+//        }
+
+        switch(requestCode) {
+            case CHOOSE_PDF_FROM_FILE:
+                if(resultCode == RESULT_OK) {
+                    String path = resultData.getData().getPath();
+                    Log.i("TEST", path);
+                    file = new File(path);
+
+                    pdfView.fromFile(file).load();
+                }
+                break;
         }
+
+
     }
+
 }
